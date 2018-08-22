e85c4e201ec38cb01f358ed4d4d39ba86b22fc33
Daniel Schoch <d.schoch90@gmail.com>
2018-08-22 19:32:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,20 +41,20 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count[genre] += 1 
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
     end
     genre_count
   end
-    
+
 end
 
 
-genre_count = Hash.new(0)
-@@genres.each do |genre|
-  genre_count.store(genre, genre_count[genre]+1)
-end
-genre_count
-end
+#genre_count = Hash.new(0)
+#@@genres.each do |genre|
+  #genre_count.store(genre, genre_count[genre]+1)
+#end
+#genre_count
+#end
