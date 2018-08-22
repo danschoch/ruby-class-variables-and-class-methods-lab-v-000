9a6bc0eea7ca81a0c53ec965f29c5bcdb5dd9885
Daniel Schoch <d.schoch90@gmail.com>
2018-08-22 19:34:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,23 +38,10 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
+    genre_count = Hash.new(0)
     @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
+      genre_count.store(genre, genre_count[genre]+1)
     end
     genre_count
   end
-
 end
-
-
-#genre_count = Hash.new(0)
-#@@genres.each do |genre|
-  #genre_count.store(genre, genre_count[genre]+1)
-#end
-#genre_count
-#end
